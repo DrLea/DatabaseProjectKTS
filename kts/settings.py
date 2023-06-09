615597b8da48c757d609bae26949092b9a28d0e6
golly47 <88594418+golly47@users.noreply.github.com>
2023-06-10 01:18:25 +0500

    fixing static

--- kts/settings.py
+++ kts/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-vad9g+un$%&z2zw0nd)d0dovz583@%)u0y#(oppq&vl2u*xu50"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
